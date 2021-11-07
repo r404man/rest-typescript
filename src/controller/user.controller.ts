544a165a3177de8eaee98fda51d1362290ce7f46
Roman <r404man.tyrtychko@gmail.com>
2021-11-07 11:24:52 +0500

    Fix bcrypt, add post api/users

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { CreateUserInput } from "../schema/user.schema";
+import { createUser } from "../services/user.service";
+import logger from '../utils/logger';
+
+export async function createUserHandler(req: Request<{}, {}, CreateUserInput['body']>, res: Response) {
+    try {
+        const user = await createUser(req.body);
+        return user
+    } catch (error: any) {
+        logger.error(error);
+        return res.status(409).send(error.message);
+    }
+}
--- src/middleware/validateResource.ts
+++ src/middleware/validateResource.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from "express"
 import { AnyZodObject } from "zod"
 
 const validate = (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) => {
-
     try {
         schema.parse({
             body: req.body,
